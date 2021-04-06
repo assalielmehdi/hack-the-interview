a9f754740b39adf51cc86d1cab77a5d0387ba95a
El Mehdi ASSALI <assalielmehdi@gmail.com>
2021-04-06 23:24:37 +0200

    Initialize project

---
+++ src/test/java/com/hacktheinterview/core/MainTests.java
@@ -0,0 +1,13 @@
+package com.hacktheinterview.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
